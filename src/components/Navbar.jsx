815fa84fb5541e8b788bddc35f7f620ad06e245d
Udeme98 <Darealudeme1@gmail.com>
2024-04-30 11:48:43 +0100

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+import { Button, Menu, Typography, Avatar } from "antd";
+import {
+  HomeOutlined,
+  MoneyCollectedOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+export default Navbar;
