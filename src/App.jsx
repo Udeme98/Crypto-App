9e1593c003e739df320a3183097fca3cc8a0c5ba
Udeme98 <Darealudeme1@gmail.com>
2024-04-30 10:01:56 +0100

    setup the different section of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 const App = () => {
-  return <div>Cryptoverse</div>;
+  return (
+    <div className="app">
+      <div className="navbar"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 export default App;
