5d34689eb37f308b4ac5113a185065c99b43553d
Udeme98 <Darealudeme1@gmail.com>
2024-05-01 05:18:52 +0100

    Navbar component: partially complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Button, Menu, Typography, Avatar } from "antd";
 import icon from "../assets/images/cryptocurrency.png";
 import {
   HomeOutlined,
+  MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
   MenuOutlined,
@@ -19,7 +20,18 @@ const Navbar = () => {
         {/* <Button className="menu-control-container"></Button> */}
       </div>
       <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}></Menu.Item>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/crytocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
       </Menu>
     </div>
   );
