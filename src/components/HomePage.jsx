cd92b64a6def4546cba28b4e56615f7efb6d58b8
Udeme98 <Darealudeme1@gmail.com>
2024-05-02 04:29:51 +0100

    HomePage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,16 @@
+import { millify } from "millify";
+import { Link } from "react-router-dom";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+    </>
+  );
 };
 export default HomePage;
