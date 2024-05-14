631746e5d9b8bb533a24ca717b278ae987777df7
Udeme98 <Darealudeme1@gmail.com>
2024-05-14 11:35:42 +0100

    setting up the request pathway

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,33 +1,19 @@
-// const options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/coins",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     timePeriod: "24h",
-//     "tiers[0]": "1",
-//     orderBy: "marketCap",
-//     orderDirection: "desc",
-//     limit: "50",
-//     offset: "0",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
-//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-//   },
-// };
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/exchanges",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     limit: "50",
-//     offset: "0",
-//     orderBy: "24hVolume",
-//     orderDirection: "desc",
-//   },
-//   headers: {
-//     "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
-//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-//   },
-// };
+const cryptoAPiHeaders = {
+  "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+const createRequest = (url) => ({ url, headers: cryptoAPiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCrypto: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
