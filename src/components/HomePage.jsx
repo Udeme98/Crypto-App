f7bce16d6dc567e8f65890a198980c0b2ad9aea7
Udeme98 <Darealudeme1@gmail.com>
2024-05-14 14:13:48 +0100

    displayed the crptos info

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,9 +10,9 @@ const HomePage = () => {
   const { data, isFetching } = useGetCryptosQuery();
   console.log(data);
 
-  // const globalStats = data?.data?.stats;
+  const globalStats = data?.data?.stats;
 
-  // if (isFetching) return "Loading...";
+  if (isFetching) return "Loading...";
 
   return (
     <>
@@ -21,21 +21,41 @@ const HomePage = () => {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={6} />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={2} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={3} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={4} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market" value={5} />
+          <Statistic
+            title="Total Market"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
     </>
   );
 };
--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,16 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const cryptoApiHeaders = {
-//   "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
-//   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-// };
 const cryptoApiHeaders = {
   "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
-// const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
-const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
@@ -24,3 +19,21 @@ export const cryptoApi = createApi({
 });
 
 export const { useGetCryptosQuery } = cryptoApi;
+
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     "tiers[0]": "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
+//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//   },
+// };
