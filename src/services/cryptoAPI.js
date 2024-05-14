feb0076912f7484bbb8cce070d20d69bc1aef4bb
Udeme98 <Darealudeme1@gmail.com>
2024-05-14 10:58:38 +0100

    working with rapidAPI: coinranking

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -15,3 +15,19 @@
 //     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 //   },
 // };
+
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/exchanges",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     limit: "50",
+//     offset: "0",
+//     orderBy: "24hVolume",
+//     orderDirection: "desc",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "d39540efe9msh46bf83225ec09f4p118cf8jsn9e7c2b4bc5c4",
+//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+//   },
+// };
