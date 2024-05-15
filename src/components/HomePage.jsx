4280f0e5d7940779aa863fb5f4ad415d5df4af2e
Udeme98 <Darealudeme1@gmail.com>
2024-05-15 05:11:02 +0100

    CryptoDetails component setup

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -59,16 +59,16 @@ const HomePage = () => {
         </Title>
       </div>
       <Cryptocurrencies simplified />
-      {/* 
-      <div className="home-heading-container">
+
+      {/* <div className="home-heading-container">
         <Title level={2} className="home-title">
           Latest Crypto News
         </Title>
         <Title level={3} className="show-more">
           <Link to="/news">Show More</Link>
         </Title>
-      </div> */}
-      {/* <News simplified /> */}
+      </div>
+  <News simplified />*/}
     </>
   );
 };
